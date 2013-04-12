02362a4e56f21b9be18c7f304bbe1df733d8d18c
David Wu <davidcnwu@gmail.com>
2013-04-12 14:58:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lemmingurl::Application.config.secret_token = 'aeac0e6e030aa8cc410be4f1edcee70f06091d5d0952f9c1cf1f28e32eed60aeae96df54bbdaaeaf40d52887b43387b9b8640e950651668be8e9daa76cb92a8e'
